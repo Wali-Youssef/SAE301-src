d70cdb2d3906972c891d3f2081bcbd9d7663941f
Wali-Youssef <youssef.wali@edu.univ-fcomte.fr>
2024-01-30 11:15:15 +0100

    test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,80 +1,61 @@
+<script>
+import { ref , defineProps } from 'vue'
+import { RouterLink } from 'vue-router';
 
+</script>
 
 <template>
-  <header class="header">
-    <RouterLink to="/" class="logo">
-      <img src="@/assets/TaVue.svg" alt="Logo" />
-    </RouterLink>
-    <nav>
-      <RouterLink to="/map" class="nav">Ou nous trouver ?</RouterLink>
-      <RouterLink to="/Personnalisation" class="nav">Personnaliser vos lunettes</RouterLink>
-    </nav>
-    <RouterLink to="/connexion" class="signup-btn">CONNECTION</RouterLink>
+ 
 
-    
-
-  </header>
+ <header class='shadow-md font-sans'>
+      <section class='py-2 bg-[#007bff] text-white text-right px-10'>
+        <p class='text-sm'><strong class="mx-2">Address:</strong>SWF New York 185669<strong class="mx-2">Contact
+            No:</strong>1800333665</p>
+      </section>
+      <div class='flex flex-wrap items-center justify-between gap-4 px-10 py-4 relative bg-white min-h-[70px]'>
+        <a href="javascript:void(0)"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" class='w-36' />
+        </a>
+        <div class='flex max-lg:ml-auto lg:order-1'>
+          <button id="toggle" class='lg:hidden'>
+            <svg class="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </button>
+        </div>
+        <ul id="collapseMenu" class='lg:!flex lg:space-x-4 max-lg:space-y-2 max-lg:hidden max-lg:py-4 max-lg:w-full'>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'>
+            <a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]'>Home</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>Team</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>Feature</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>Blog</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>About</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>Contact</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>Source</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>Partner</a>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2 px-3 max-lg:rounded'><a href='javascript:void(0)'
+              class='lg:hover:text-[#007bff] text-[#333] block font-semibold text-[15px]'>More</a>
+          </li>
+        </ul>
+      </div>
+    </header>
 </template>
 
 
-
-<style scoped>
-.header {
-  background-color: #FFFBF5;
-  height: 85px;
-  max-width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
-  font-family: 'Montserrat';
-  
-}
-
-.logo img {
-  width: 120px;
-
-}
-
-nav {
-  display: flex;
-  gap: 180px;
-
-
-}
-
-.nav-btn,
-.signup-btn {
-  background-color: #11485F;
-  color: white;
-  border: none;
-  border-radius: 18px;
-  padding: 10px 20px;
-  text-decoration: none;
-  font-family: 'PT Sans', sans-serif;
-  font-size: 17px;
-  font-weight: 400;
-  cursor: pointer;
-  transition: background-color 0.3s;
-}
-
-.nav-btn:hover,
-.signup-btn:hover {
-  background-color: #2c0b7a;
-}
-
-
-nav .nav,
-.signup-btn {
-  text-decoration: none;
-  /* This will remove the underline */
-
-}
-
-.nav {
-  color: #11485F;
-
-
-}
-</style>
-  
